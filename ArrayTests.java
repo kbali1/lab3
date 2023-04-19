43cd1447f0300ccf3a2b458df62e09c3e411ce70
kbali1 <122485688+kbali1@users.noreply.github.com>
2023-04-19 16:26:48 -0700

    Fixed methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,9 +4,9 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = { 3, 2, 1 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{ 1, 2, 3}, input1);
 	}
 
 
@@ -15,4 +15,19 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+
+@Test
+public void testReversed2(){
+  int[] input2 = {1, 2, 3};
+  assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input2));
+}
+
+
+@Test
+public void testwithoutLowest(){
+  assertEquals(0, ArrayExamples.averageWithoutLowest({0}));
+
+}
+
 }
